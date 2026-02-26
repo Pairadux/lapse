770afdc709c34df97775445b14d98cec8e941faf
Germaine Dudley <115107626+GADudley@users.noreply.github.com>
2026-02-26 17:21:29 -0500

    fixed various errors (#73)

--- lib/features/cards/presentation/providers/card_list_provider.dart
+++ lib/features/cards/presentation/providers/card_list_provider.dart
@@ -3,39 +3,40 @@ import 'package:lapse/features/cards/data/card_repository_provider.dart';
 import 'package:lapse/features/cards/domain/flashcard.dart';
 import 'package:lapse/features/decks/presentation/providers/deck_list_provider.dart';
 
-final cardListProvider = AsyncNotifierProvider.family<
-    CardListNotifier, List<Flashcard>, String>(
-  CardListNotifier.new,
+final cardListProvider = AsyncNotifierProvider.family<CardListNotifier, List<Flashcard>, String>(
+  (arg) => CardListNotifier(arg),
 );
 
-class CardListNotifier extends FamilyAsyncNotifier<List<Flashcard>, String> {
+class CardListNotifier extends AsyncNotifier<List<Flashcard>> {
+  CardListNotifier(this.deckId);
+  final String deckId;
 
   @override
-  Future<List<Flashcard>> build(String deckId) {
-    return ref.read(cardRepositoryProvider).getByDeckId(deckId);
+  Future<List<Flashcard>> build() {
+    return ref.watch(cardRepositoryProvider).getByDeckId(deckId);
   }
 
   Future<void> createCard(Flashcard card) async {
-    state = await AsyncValue.guard<List<Flashcard>>(() async {
+    state = await AsyncValue.guard(() async {
       await ref.read(cardRepositoryProvider).create(card);
-      ref.invalidate(deckListProvider);
-      return ref.read(cardRepositoryProvider).getByDeckId(arg);
+      return ref.read(cardRepositoryProvider).getByDeckId(card.deckId);
     });
+    ref.invalidate(deckListProvider);
   }
 
   Future<void> updateCard(Flashcard card) async {
-    state = await AsyncValue.guard<List<Flashcard>>(() async {
+    state = await AsyncValue.guard(() async {
       await ref.read(cardRepositoryProvider).update(card);
-      ref.invalidate(deckListProvider);
-      return ref.read(cardRepositoryProvider).getByDeckId(arg);
+      return ref.read(cardRepositoryProvider).getByDeckId(card.deckId);
     });
+    ref.invalidate(deckListProvider);
   }
 
   Future<void> deleteCard(String cardId) async {
-    state = await AsyncValue.guard<List<Flashcard>>(() async {
+    state = await AsyncValue.guard(() async {
       await ref.read(cardRepositoryProvider).delete(cardId);
-      ref.invalidate(deckListProvider);
-      return ref.read(cardRepositoryProvider).getByDeckId(arg);
+      return ref.read(cardRepositoryProvider).getByDeckId(deckId);
     });
+    ref.invalidate(deckListProvider);
   }
 }
